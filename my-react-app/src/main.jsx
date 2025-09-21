c07e8ceca8fe45534710ccef37ce53c1308cf08e
aloopday <aloopday@gmail.com>
2025-09-21 10:53:04 +0800

    add some fiels

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { createRoot } from 'react-dom/client'
+
+/*
+const myelement =(
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>Charles</td>
+
+    </tr>
+    <tr>
+      <td>Wang</td>
+    </tr>
+  </table>
+)
+*/
+
+createRoot(document.getElementById('header')).render(
+ <p>Welcome to Hong Kong !</p>
+
 )
